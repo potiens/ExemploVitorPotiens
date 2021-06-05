ab3c06926c96eb7fe4d056d39d958b3c7fcbecb6
vitor <vitor@192.168.15.8>
2021-06-04 21:06:03 -0300

    primeiro commit

---
+++ src/exemplovitorpotiens/ExemploVitorPotiens.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemplovitorpotiens;
+
+/**
+ *
+ * @author vitor
+ */
+public class ExemploVitorPotiens {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Exemplo de um projeto qualquer com git e netbeans");
+    }
+    
+}
